e95c79d813eda950193d8cd6436f9187d29cce3d
kristellu <kristellu@uninorte.edu.co>
2021-11-28 15:28:28 -0500

    Added second part

--- server.js
+++ server.js
@@ -1,11 +1,17 @@
+const op = require("./operaciones.js");
+
 // server.js
-"use strict";
+("use strict");
 var http = require("http");
 
 var server = http.createServer(function (req, res) {
   res.writeHead(200, { "content-type": "text/plain" });
   res.end("Hola Mundo");
 });
+
+// console.log("RESULTADO", op.suma(3, 4));
+
+console.log("Nombre es:", op.getNombre());
 
 //Para coloar el servidor en la red, se envía el puerto
 server.listen(8080);
